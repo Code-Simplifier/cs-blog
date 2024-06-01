ddbe092c43109006be4b3b7252ecc791b7b95c3e
codesimplifier <codesimplifier@gmail.com>
2024-06-01 09:28:51 +0530

    v0.5.3 - Custom Error Page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+
+	import Button from "../components/shared/button.svelte";
+
+</script>
+<section class="h-screen flex flex-col items-center justify-center">
+    <span class="title-bold tracking-wide text-error text-6xl">Oops!</span>
+    <p class="content my-2 text-center px-5 text-lg md:text-2xl">We couldnt find the page you were looking for 😔</p>
+    <Button url="/" title="Home" />
+</section>
