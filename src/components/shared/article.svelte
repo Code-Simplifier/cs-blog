abbfc9e37df8519766e268af3b1e13d1e72ac31b
codesimplifier <codesimplifier@gmail.com>
2024-06-01 12:23:42 +0530

    v0.6.1 - Code Optimization

---
+++ src/components/shared/article.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { Tags } from "$lib/types";
+
+	export let article: {
+		slug: string;
+		title: string;
+		desc: string;
+		tags: Tags[];
+		thumbnail: string;
+	};
+</script>
+
+<a href={`/articles/${article.slug}`}>
+	<article
+		class="relative h-[450px] w-[350px] rounded-2xl bg-secondary duration-200 hover:mx-5 hover:my-3 hover:scale-105 hover:border-2 hover:border-slate-500"
+	>
+		<img src={article.thumbnail} alt={article.title} class="rounded-t-2xl" />
+
+		<div class="flex w-[90%] flex-col justify-between p-4">
+			<div class="flex flex-col gap-y-3">
+				<h1 class="title-semi text-2xl uppercase tracking-wide text-primary">
+					{article.title}
+				</h1>
+				<span>{article.desc}</span>
+			</div>
+
+			<div class="absolute bottom-3 flex gap-x-2">
+				{#each article.tags as tag}
+					<div class={`code rounded-xl px-2 ${tag.bgColor} ${tag.fgColor}`}>
+						#{tag.title}
+					</div>
+				{/each}
+			</div>
+		</div>
+	</article>
+</a>
--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	export let data;
+	import Article from "../../components/shared/article.svelte";
 </script>
 
 <svelte:head>
@@ -14,37 +15,9 @@
 	<div class="my-10 w-[10%] rounded-xl bg-slate-800 p-1" />
 	<div class="grid grid-cols-1 place-items-center gap-3 md:grid-cols-3">
 		{#each data.articles as { title, slug, desc, tags, thumbnail }}
-			<a href={`/articles/${slug}`}>
-				<div
-					class="relative h-[450px] w-[350px] rounded-2xl bg-secondary duration-200 hover:mx-5 hover:my-3 hover:scale-105 hover:border-2 hover:border-slate-500"
-				>
-					<img src={thumbnail} alt={title} class="rounded-t-2xl" />
-					<div class="flex w-[90%] flex-col justify-between p-4">
-						<div class="flex flex-col gap-y-3">
-							<h1 class="title-semi text-2xl uppercase tracking-wide text-primary">
-								{title}
-							</h1>
-							<span>{desc}</span>
-						</div>
-						<div class="absolute bottom-3 flex gap-x-2">
-							{#each tags as tag}
-								<div class={`code rounded-xl px-2 ${tag.bgColor} ${tag.fgColor}`}>
-									#{tag.title}
-								</div>
-							{/each}
-						</div>
-					</div>
-				</div>
-			</a>
+			<Article article={{ title, slug, desc, tags, thumbnail }} />
 		{/each}
 	</div>
 </section>
 
-<style>
-	img {
-		view-timeline-name: --var(image);
-	}
-	h1 {
-		view-timeline-name: --var(title);
-	}
-</style>
+
